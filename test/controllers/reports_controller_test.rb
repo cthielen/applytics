06170774c658543270fcaa743bc590657217a528
Christopher Thielen <christopher@thielen.co>
2016-08-01 13:29:35 -0700

    Add TODOs for the ReportsController test.

--- test/controllers/reports_controller_test.rb
+++ test/controllers/reports_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class ReportsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # TODO: Write me.
+  test "top_urls" do
+    assert false
+  end
+
+  # TODO: Write me.
+  test "top_referrers" do
+    assert false
+  end
 end
