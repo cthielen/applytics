faaa2b67feebad93463ac9da1b3d2c4f670e77da
Christopher Thielen <christopher@thielen.co>
2016-08-15 23:31:14 -0700

    Finish Rails JSON tests.

--- test/controllers/reports_controller_test.rb
+++ test/controllers/reports_controller_test.rb
@@ -6,11 +6,34 @@ class ReportsControllerTest < ActionController::TestCase
     get :top_urls, { :format => 'json' }
     assert_response :success
     assert_not_nil assigns(:logs)
+
+    body = JSON.parse(response.body)
+
+    body.each do |day, entries|
+      entries.each do |entry|
+        assert entry.include?('url'), 'JSON response should include url field'
+        assert entry.include?('visits'), 'JSON response should include visits field'
+      end
+    end
   end
 
   test "top_referrers" do
     get :top_referrers, { :format => 'json' }
     assert_response :success
     assert_not_nil assigns(:logs)
+
+    body = JSON.parse(response.body)
+
+    body.each do |day, entries|
+      entries.each do |entry|
+        assert entry.include?('url'), 'JSON response should include url field'
+        assert entry.include?('visits'), 'JSON response should include visits field'
+        assert entry.include?('referrers'), 'JSON response should include referrers field'
+        entry['referrers'].each do |referrer|
+          assert referrer.include?('url'), 'JSON response should include url field'
+          assert referrer.include?('visits'), 'JSON response should include visits field'
+        end
+      end
+    end
   end
 end
